482b15f81d3fe4fb936bbbb6fd523f056b834723
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-03-26 17:13:08 -0400

    Improve CRUD message and errors
    Made all input fields require a minimum of 1 character. This includes
    the auth inputs.
    Edited success/failure messages for resources' CRUD for accuracy.

--- assets/scripts/crud/ui.js
+++ assets/scripts/crud/ui.js
@@ -12,32 +12,32 @@ const indexSuccess = (data) => {
 
 const createSuccess = (data) => {
   console.log(data)
-  $('.home-view-message').text('You have successfully created.')
+  $('.home-view-message').text('You have successfully added your gear.')
 }
 
 const createFailure = (error) => {
   console.log(error)
-  $('.home-view-message').text('You have not successfully created.')
+  $('.home-view-message').text('You have not successfully created your gear.')
 }
 
 const updateSuccess = (data) => {
   console.log(data)
-  $('.home-view-message').text('You have successfully updated.')
+  $('.home-view-message').text('You have successfully updated your gear.')
 }
 
 const updateFailure = (error) => {
   console.log(error)
-  $('.home-view-message').text('You have not successfully updated.')
+  $('.home-view-message').text('You have not successfully updated your gear.')
 }
 
 const destroySuccess = (data) => {
   console.log(data)
-  $('.home-view-message').text('You have successfully deleted.')
+  $('.home-view-message').text('You have successfully deleted your gear.')
 }
 
 const destroyFailure = (error) => {
   console.log(error)
-  $('.home-view-message').text('You have not successfully deleted.')
+  $('.home-view-message').text('You have not successfully deleted your gear.')
 }
 
 module.exports = {
